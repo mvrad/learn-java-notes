5f026e6793707eac9aafd78f56dc908d70106cb5
Matthew Conrad <matthew.conrad@outlook.com>
2022-04-22 20:48:42 -0700

    Minor edit

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -4,11 +4,11 @@ public class Variables {
 
     public static void main(String[] args) {
         
-        byte b=5;
+        byte  b=5;
         short s=300;
-        int i=100;
+        int   i=100;
         float f=25.3f;
-        char c='A';
+        char  c='A';
         
         System.out.println(b);
         System.out.println(s);
